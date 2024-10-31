81f674e33d61e0d21ee7941ebd5fdad6e8cb27ff
Lokesh <lokeshwardevanand@gmail.com>
2024-10-31 23:38:07 +0100

    Optimzed for test03

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import itertools
 import sys
+from collections import defaultdict
 
 # generate bit strings with N-1 ones
-def generate_bit_strings(num_edges, num_vertices):   
+def generate_bit_strings(num_edges, num_vertices, edge_number):   
     
-    required_ones = num_vertices - 1 
+    # required_ones = num_vertices - 1 #- len(non_cycle_edges)              # Old version of the code
+    
+    # for bit_string_tuple in itertools.combinations(range(num_edges), required_ones):
+    #     bit_string = [0] * num_edges
+
+        
+    #     for pos in bit_string_tuple:
+    #         bit_string[pos] = 1
+        
+    #     yield bit_string
+
+    required_ones = num_vertices - 1 - len(edge_number)
+    num_edges = num_edges - len(edge_number)
 
     for bit_string_tuple in itertools.combinations(range(num_edges), required_ones):
         bit_string = [0] * num_edges
+
         
         for pos in bit_string_tuple:
             bit_string[pos] = 1
         
+        for i in edge_number:
+            bit_string.insert(i, 1)
+       
         yield bit_string
+    
 
 
 def read_input():
@@ -109,21 +127,120 @@ def check_spanning_tree(bit_string, edges, num_vertices, best_weight, indicator)
       
     return True, total_weight, mirror_weight, best_weight,EDGEnumber
 
+
+def dfs_cycle(u, p, color, mark, parents, adjList, cyclenumber, cycles):
+    if color[u] == 2:  # Fully visited
+        return
+    color[u] = 1  # Partially visited
+    for v in adjList[u]:
+        if v == p:
+            continue
+        if color[v] == 0:
+            parents[v] = u
+            dfs_cycle(v, u, color, mark, parents, adjList, cyclenumber, cycles)
+        elif color[v] == 1:  # Found a back edge, indicating a cycle
+            cyclenumber[0] += 1
+            cycle = []
+            cur = u
+            while cur != v:
+                mark[cur] = cyclenumber[0]
+                cycle.append(cur)
+                cur = parents[cur]
+            cycle.append(v)
+            cycle.append(u)
+            for node in cycle:
+                mark[node] = cyclenumber[0]
+            cycles.append(cycle)
+    color[u] = 2  # Fully visited
+
+def find_cycles(num_vertices, edges):
+    adjList = defaultdict(list)
+    for u, v, _ in edges:
+        adjList[u].append(v)
+        adjList[v].append(u)
+
+    color = [0] * (num_vertices + 1)
+    parents = [-1] * (num_vertices + 1)
+    mark = [0] * (num_vertices + 1)
+    cyclenumber = [0]
+    cycles = []
+
+    for i in range(1, num_vertices + 1):
+        if color[i] == 0:
+            dfs_cycle(i, -1, color, mark, parents, adjList, cyclenumber, cycles)
+    
+    return cycles
+
+def get_non_cycle_edges(num_vertices, edges):
+    cycles = find_cycles(num_vertices, edges)
+    cycle_edges = set()
+    for cycle in cycles:
+        for i in range(len(cycle) - 1):
+            u, v = cycle[i], cycle[i + 1]
+            cycle_edges.add(frozenset({u, v}))
+
+    non_cycle_edges = []
+    for u, v, weight in edges:
+        if frozenset({u, v}) not in cycle_edges:
+            non_cycle_edges.append((u, v, weight))
+
+    return non_cycle_edges
+
+# def find_leaf_vertices(num_vertices, edges):                  # possible idea to optimize the code but not used
+#     # Step 1: Build the adjacency list and count degrees
+#     adjacency_list = {i: [] for i in range(1, num_vertices + 1)}
+#     degree = {i: 0 for i in range(1, num_vertices + 1)}
+    
+#     for u, v, weight in edges:
+#         adjacency_list[u].append(v)
+#         adjacency_list[v].append(u)
+#         degree[u] += 1
+#         degree[v] += 1
+#     #print("Adjacency list:", adjacency_list)
+#     #print("Degree:", degree)
+#     # Step 2: Identify leaf vertices (vertices with degree 1)
+#     leaf_vertices = [vertex for vertex in degree if degree[vertex] == 1]
+#     #print("Leaf vertices:", leaf_vertices)
+
+#     return leaf_vertices
+
+def find_edge_number(edges, non_cycle_edges):
+    edge_number = []
+    for edge in non_cycle_edges:
+        for i, e in enumerate(edges):
+            if e == edge:
+                edge_number.append(i)
+                break
+    return edge_number
+
+# def edge_match(edge1, edge2):     # possible idea to optimize the code but not used
+#     for i in edge2:
+#         if i not in edge1:
+#             edge1.append(i)
+
+#     edge1.sort()
+#     return edge1
+
 #Main code
 n, m, edges = read_input()
 best_weight=float('inf') # the b value which is the best weight
 indicator = int(0)
 best_egdes = [] # edge tuple values that give the best weight
 best_egdes_number = [] # egdes that give the best weight
-
-
-for valid_bit_string in generate_bit_strings(m, n):
+#edge_number=[]
+non_cycle_edges = get_non_cycle_edges(n, edges)     #gets the confirmed edges
+edge_number =find_edge_number(edges, non_cycle_edges)   #gets the edge number for the confirmed edges
+#leaf_vertices= find_leaf_vertices(n, edges)
+#leaf_number= find_edge_number(edges, leaf_vertices)
+#edge_number = edge_match(edge_number, leaf_number)
+#print(edge_number, "edge_number and non_cycle_edges", non_cycle_edges)
+for valid_bit_string in generate_bit_strings(m, n, edge_number):
    
-    # Check the spanning tree and calculate the weights
-    is_valid, total_weight, mirror_weight, best_weight,EDGEnumber = check_spanning_tree(valid_bit_string, edges, n, best_weight, indicator)
+     # Check the spanning tree and calculate the weights
+     is_valid, total_weight, mirror_weight, best_weight,EDGEnumber = check_spanning_tree(valid_bit_string, edges, n, best_weight, indicator)
    
     
-    if EDGEnumber != [] and EDGEnumber != [0]:
+     if EDGEnumber != [] and EDGEnumber != [0]:
         best_egdes_number = EDGEnumber[:]
 
 #The main final output
